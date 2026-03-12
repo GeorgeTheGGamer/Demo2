0f4ea8a912f4a6bc643a8de9913fb2afc870c72c
BAsComahh <3160756128@qq.com>
2026-03-12 19:40:24 +0000

    change root path, so models can be read

--- Demo3/laptop_server_new.py
+++ Demo3/laptop_server_new.py
@@ -1,6 +1,12 @@
 import importlib
 import threading
 import time
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 
 import Demo3.states.globals as g
 from Demo3.connection.commands import (
